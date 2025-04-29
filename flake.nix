4f18f694a9c965c8d89354c8ed9c1919d4cb5603
Olivier Bitter <olibitter@gmail.com>
2025-04-29 20:04:30 +0200

    feat: setup simple flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A flake providing my zsh config.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+
+    packages.x86_64-linux.blubb = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
