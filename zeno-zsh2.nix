7fec775d65d8f663ccedef6eb4ba82a91bf7bb92
Olivier Bitter <olibitter@gmail.com>
2025-11-02 22:01:01 +0100

    formatting

--- zeno-zsh2.nix
+++ zeno-zsh2.nix
@@ -1,63 +1,66 @@
 { wlib, lib }:
-  wlib.wrapModule ({ config, wlib, ... }: 
-    let
-      pkgs = config.pkgs;
-      pluginSpecs = [
-        {
-          name = "fzf-tab";
-          plugin = pkgs.zsh-fzf-tab;
-        }
-        rec {
-          name = "zsh-vi-mode";
-          plugin = pkgs.zsh-vi-mode;
-          config = ''
-            zvm_after_init_commands+=('source <(fzf --zsh)')
-          '';
-        }
-      ];
-      zshPlugins = lib.concatMapStringsSep "\n" (
-        spec:
-        builtins.concatStringsSep "\n" (
-          [
-            ''
-              ##########################
-              ## ${spec.name}
-              ##########################
-              source ${spec.plugin.src}/${spec.name}.plugin.zsh
-            ''
-          ]
-          ++ (lib.optionals (builtins.hasAttr "config" spec) [
-            "# Config"
-            spec.config
-          ])
-        )
-      )
-      pluginSpecs;
-      zdotdir = config.pkgs.writeTextFile {
-        name = "zdotdir";
-        text = ''
-          # Sources a file relative to the src directory
-          function load {
-            source ${./src}/$1
-          }
-
-          # Cleans up the environment
-          function cleanup {
-            unfunction load
-            # unset ZDOTDIR
-          }
-
-          load init.zsh
-
-          cleanup
+wlib.wrapModule (
+  { config, wlib, ... }:
+  let
+    pkgs = config.pkgs;
+    pluginSpecs = [
+      {
+        name = "fzf-tab";
+        plugin = pkgs.zsh-fzf-tab;
+      }
+      rec {
+        name = "zsh-vi-mode";
+        plugin = pkgs.zsh-vi-mode;
+        config = ''
+          zvm_after_init_commands+=('source <(fzf --zsh)')
         '';
-        destination = "/.zshrc";
-      };
-    in 
-    {
+      }
+    ];
+    zshPlugins = lib.concatMapStringsSep "\n" (
+      spec:
+      builtins.concatStringsSep "\n" (
+        [
+          ''
+            ##########################
+            ## ${spec.name}
+            ##########################
+            source ${spec.plugin.src}/${spec.name}.plugin.zsh
+          ''
+        ]
+        ++ (lib.optionals (builtins.hasAttr "config" spec) [
+          "# Config"
+          spec.config
+        ])
+      )
+    ) pluginSpecs;
+    zdotdir = config.pkgs.writeTextFile {
+      name = "zdotdir";
+      text = ''
+        # Sources a file relative to the src directory
+        function load {
+          source ${./src}/$1
+        }
+
+        # Cleans up the environment
+        function cleanup {
+          unfunction load
+          # unset ZDOTDIR
+        }
+
+        load init.zsh
+
+        cleanup
+      '';
+      destination = "/.zshrc";
+    };
+  in
+  {
     options = {
       profile = lib.mkOption {
-        type = lib.types.enum [ "fast" "quality" ];
+        type = lib.types.enum [
+          "fast"
+          "quality"
+        ];
         default = "fast";
         description = "Encoding profile to use";
       };
@@ -67,11 +70,12 @@
         description = "Directory for output files";
       };
     };
-      config = {
-        package = config.pkgs.zsh;
-        extraPackages = with pkgs; [ starship ];
-        env = {
-          ZDOTDIR = "${zdotdir}";
-        };
+    config = {
+      package = config.pkgs.zsh;
+      extraPackages = with pkgs; [ starship ];
+      env = {
+        ZDOTDIR = "${zdotdir}";
       };
-    })
+    };
+  }
+)
