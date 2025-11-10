8c55ee6f83c86660ecb4b1bddab0cb599f9a246c
Olivier Bitter <olibitter@gmail.com>
2025-11-10 19:17:48 +0100

    cleanup
    Choose lassulus wrapper lib for now.
    Remove other drafts

--- flake.nix
+++ flake.nix
@@ -3,50 +3,39 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     wrappers.url = "github:lassulus/wrappers";
-    wrapper-manager.url = "github:viperML/wrapper-manager";
   };
 
-  outputs = { self, nixpkgs, home-manager, wrappers, wrapper-manager }: 
-  let
-    util = import ./nix/util.nix { inherit nixpkgs; };
-    loadModule = moduleFn:
-    (moduleFn { 
-      wlib = wrappers.lib; 
-      lib = nixpkgs.lib; 
-    });
-    applyWrapperModule = moduleFn: pkgs: args:
-      ((loadModule moduleFn).apply ({ inherit pkgs; } // args)).wrapper;
-    evalWrapperConfig = moduleFn: pkgs: args:
-      ((loadModule moduleFn).apply ({ inherit pkgs; } // args));
-  in 
-  {
-    packages = util.forAllSystems (pkgs: 
-    let 
-      wm-eval = wrapper-manager.lib {
-        inherit pkgs;
-        modules = [ ./zeno-zsh3.nix ];
-      };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      wrappers,
+    }:
+    let
+      util = import ./nix/util.nix { inherit nixpkgs; };
+      loadModule =
+        moduleFn:
+        (moduleFn {
+          wlib = wrappers.lib;
+          lib = nixpkgs.lib;
+        });
+      applyWrapperModule =
+        moduleFn: pkgs: args:
+        ((loadModule moduleFn).apply ({ inherit pkgs; } // args)).wrapper;
     in
     {
-      default = pkgs.callPackage ./zeno-zsh.nix {};
-      ghd = pkgs.callPackage ./scripts/ghd {};
-      zsh2 = applyWrapperModule (import ./zeno-zsh2.nix) pkgs { direnv = false; };
-      zsh3 = wm-eval.config.wrappers.zsh.wrapped;
-      zsh4 = wrapper-manager.lib.wrapWith pkgs (import ./zeno-zsh4.nix);
+      wrapperModules = {
+        zsh = import ./zeno-zsh.nix;
+      };
 
-    });
+      packages = util.forAllSystems (pkgs: {
+        default = applyWrapperModule self.wrapperModules.zsh pkgs (import ./zsh-config.nix);
+        ghd = pkgs.callPackage ./scripts/ghd { };
+      });
 
-    devShells = util.forAllSystems (pkgs: {
-      default = import ./nix/shell.nix { inherit pkgs; };
-    });
-
-    zshConfig = evalWrapperConfig (import ./zeno-zsh2.nix) nixpkgs.legacyPackages."x86_64-linux" ./zsh-config.nix;
-
-    homeManagerModules.zenoZsh = (import ./zeno-zsh-module.nix);
-  };
+      devShells = util.forAllSystems (pkgs: {
+        default = import ./nix/shell.nix { inherit pkgs; };
+      });
+    };
 }
--- zeno-zsh.nix
+++ zeno-zsh.nix
@@ -1,102 +1,76 @@
-{
-  # build dependencies
-  lib,
-  callPackage,
-  writeTextFile,
-  writeShellApplication,
-  makeWrapper,
-  stdenv,
-  symlinkJoin,
-  writeText,
-
-  # programs
-  fzf,
-  gh,
-  git,
-  jq,
-  starship,
-  zsh,
-  direnv,
-
-  # Zsh Plugins
-  zsh-autopair,
-  zsh-fzf-tab,
-  zsh-vi-mode,
-}:
-let
-  ghd = callPackage ./scripts/ghd {};
-  pluginSpecs = [
-    {
-      name = "fzf-tab";
-      plugin = zsh-fzf-tab;
-    }
-    rec {
-      name = "zsh-vi-mode";
-      plugin = zsh-vi-mode;
-      config = ''
-        zvm_after_init_commands+=('source <(fzf --zsh)')
-      '';
-    }
-  ];
-  zshPlugins = lib.concatMapStringsSep "\n" (
-    spec:
-    builtins.concatStringsSep "\n" (
-      [
-        ''
-          ##########################
-          ## ${spec.name}
-          ##########################
-          source ${spec.plugin.src}/${spec.name}.plugin.zsh
-        ''
-      ]
-      ++ (lib.optionals (builtins.hasAttr "config" spec) [
-        "# Config"
-        spec.config
-      ])
-    )
-  )
-  pluginSpecs;
-
-  zdotdir = writeTextFile {
-    name = "zdotdir";
-    text = ''
-      # Sources a file relative to the src directory
-      function load {
-        source ${./src}/$1
+{ wlib, lib }:
+wlib.wrapModule (
+  { config, wlib, ... }:
+  let
+    pkgs = config.pkgs;
+    pluginSpecs = [
+      {
+        name = "fzf-tab";
+        plugin = pkgs.zsh-fzf-tab;
       }
-
-      # Cleans up the environment
-      function cleanup {
-        unfunction load
-        # unset ZDOTDIR
+      rec {
+        name = "zsh-vi-mode";
+        plugin = pkgs.zsh-vi-mode;
+        config = ''
+          zvm_after_init_commands+=('source <(fzf --zsh)')
+        '';
       }
-
-      ${zshPlugins}
-
-      load init.zsh
-
-      echo "zsh1"
-
-      cleanup
-    '';
-    destination = "/.zshrc";
-  };
-in
-symlinkJoin {
-  name = "zsh";
-  buildInputs = [ makeWrapper ];
-  paths = [ zsh ];
-  postBuild = ''
-    wrapProgram $out/bin/zsh \
-      --set ZDOTDIR ${zdotdir} \
-      --prefix PATH : ${
-        lib.makeBinPath [
-          starship
-          fzf
-          ghd
-          direnv
+    ];
+    zshPlugins = lib.concatMapStringsSep "\n" (
+      spec:
+      builtins.concatStringsSep "\n" (
+        [
+          ''
+            ##########################
+            ## ${spec.name}
+            ##########################
+            source ${spec.plugin.src}/${spec.name}.plugin.zsh
+          ''
         ]
-      }
-  '';
-}
+        ++ (lib.optionals (builtins.hasAttr "config" spec) [
+          "# Config"
+          spec.config
+        ])
+      )
+    ) pluginSpecs;
+    zdotdir = config.pkgs.writeTextFile {
+      name = "zdotdir";
+      text = ''
+        # Sources a file relative to the src directory
+        function load {
+          source ${./src}/$1
+        }
 
+        # Cleans up the environment
+        function cleanup {
+          unfunction load
+          # unset ZDOTDIR
+        }
+
+        ${lib.optionalString config.direnv ''
+          echo "Foo feature is enabled!"
+        ''}
+
+        load init.zsh
+
+        cleanup
+      '';
+      destination = "/.zshrc";
+    };
+  in
+  {
+    options = {
+      direnv = lib.mkEnableOption "direnv integration";
+    };
+    config = {
+      package = config.pkgs.zsh;
+      extraPackages = with pkgs; 
+        [ starship ] 
+        ++ lib.optional config.direnv cowsay;
+      env = {
+        ZDOTDIR = "${zdotdir}";
+      };
+      direnv = lib.mkDefault true;
+    };
+  }
+)
