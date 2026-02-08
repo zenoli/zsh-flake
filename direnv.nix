43c5c0be3a5ddcdc683543aeaac06d77b5c5bbf7
Olivier Bitter <olibitter@gmail.com>
2026-02-08 18:03:37 +0100

    integrate nix-direnv

--- direnv.nix
+++ direnv.nix
@@ -1,28 +1,40 @@
-{ wlib, lib }:
-wlib.wrapModule (
-  { config, wlib, ... }:
-  let
-    pkgs = config.pkgs;
-    cfg = config;
-  in
-  {
-    options = {
-      direnvrc = lib.mkOption {
-        type = wlib.types.file pkgs;
-        default = {
-          content = "";
-        };
+{ config, lib, wlib, ... }:
+let
+  pkgs = config.pkgs;
+  cfg = config;
+in
+{
+  _class = "wrapper";
+  options = {
+    direnvrc = lib.mkOption {
+      type = wlib.types.file pkgs;
+      default = {
+        content = "";
       };
     };
-    config = {
-      package = pkgs.direnv;
-      env = { DIRENV_CONFIG = "${cfg.direnvrc.path}/direnv"; };
-      direnvrc = {
-        content = ''
-          source ${pkgs.nix-direnv}/share/nix-direnv/direnvrc
-        '';
-        path = pkgs.writeTextDir "direnv/direnvrc" cfg.direnvrc.content;
+    nix-direnv = {
+      enable = lib.mkEnableOption "nix-direnv integration";
+      package = lib.mkPackageOption pkgs "nix-direnv" { };
+    };
+  };
+  config = {
+    package = pkgs.direnv;
+    env = { DIRENV_CONFIG = "${cfg.direnvrc.path}"; };
+    direnvrc = {
+      content = ''
+        ${lib.optionalString cfg.nix-direnv.enable ''
+          source ${cfg.nix-direnv.package}/share/nix-direnv/direnvrc
+        ''
+        }
+      '';
+      # Creates a derivation "direnv-config" holding "direnvrc" as its single file:
+      # direnv-config/
+      #   direnvrc
+      path = pkgs.writeTextFile {
+        name = "direnv-config";
+        destination = "/direnvrc";
+        text = cfg.direnvrc.content;
       };
     };
-  }
-)
+  };
+}
