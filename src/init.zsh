f2a3c7abbf575ce9b4013c84c36f34b058bd92a9
Olivier Bitter <olibitter@gmail.com>
2026-02-10 23:23:47 +0100

    configure plugins in flake.nix

--- src/init.zsh
+++ src/init.zsh
@@ -2,8 +2,11 @@ autoload -U compinit && compinit
 
 
 KEYTIMEOUT=1
-VI_MODE_SET_CURSOR=true
-VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
+# VI_MODE_SET_CURSOR=true
+# VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
+
+autoload -Uz edit-command-line
+zle -N edit-command-line
 bindkey -M vicmd '^f' edit-command-line
 bindkey -M viins '^f' edit-command-line
 
