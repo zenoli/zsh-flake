d30a14f2497799a339a89e8f834d389e2ffe7696
Olivier Bitter <olibitter@gmail.com>
2026-02-14 17:20:08 +0100

    add completions

--- src/init.zsh
+++ src/init.zsh
@@ -1,6 +1,3 @@
-autoload -U compinit && compinit
-
-
 KEYTIMEOUT=1
 # VI_MODE_SET_CURSOR=true
 # VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
--- zeno-zsh.nix
+++ zeno-zsh.nix
@@ -66,6 +66,18 @@ let
         # unset ZDOTDIR
       }
 
+      # Completion
+
+      local zsh_cache_dir="$XDG_CACHE_HOME/zsh"
+      if [[ ! -d $zsh_cache_dir ]]; then
+          echo "Creating $zsh_cache_dir"
+          mkdir -p $zsh_cache_dir
+      fi
+      zcompdump_file="$zsh_cache_dir/zcompdump"
+
+      autoload -U compinit && compinit -d $zcompdump_file
+
+
       # Plugins
 
       ${zshPluginConfigs}
