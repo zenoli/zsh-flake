d69d8b2d5fbaeebf99f5143afc41689b5a3106da
Olivier Bitter <olibitter@gmail.com>
2026-02-20 18:20:01 +0100

    set HISTFILE

--- src/init.zsh
+++ src/init.zsh
@@ -7,4 +7,11 @@ zle -N edit-command-line
 bindkey -M vicmd '^f' edit-command-line
 bindkey -M viins '^f' edit-command-line
 
+
+
+zsh_state_dir="${XDG_STATE_HOME:-$HOME/.local/state}/zsh"
+mkdir -p "$zsh_state_dir"
+
+export HISTFILE="$zsh_state_dir/zsh_history"
+
 load options.zsh
