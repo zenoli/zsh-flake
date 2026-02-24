6026667b60a3104aa39e150dcc063ebb9fdb0f09
Olivier Bitter <olibitter@gmail.com>
2026-02-24 23:52:41 +0100

    use imports relative to flake root

--- modules/wrappers.nix
+++ modules/wrappers.nix
@@ -1,42 +1,21 @@
 { self, withSystem, ... }: {
   flake.wrappers = {
     direnv = { pkgs, wlib, ... }: {
-      imports = [ (import ../wrapper-modules/direnv.nix) ];
+      imports = [ (import (self + /wrapper-modules/direnv.nix)) ];
       nix-direnv.enable = true;
     };
-    zsh = { pkgs, wlib, lib, ... }: {
-      imports = [ (import ../wrapper-modules/zsh) ];
-      hmSessionVariables = {
-        enable = lib.mkDefault true;
-      };
-      starship = {
-        enable = lib.mkDefault true;
-      };
-      direnv = {
-        enable = lib.mkDefault true;
-        package = withSystem pkgs.stdenv.hostPlatform.system (
-          { config, ... }: config.packages.direnv
-        );
-      };
-      fzf = {
-        enable = lib.mkDefault true;
-      };
-      plugins = [ 
-        { 
-          package = pkgs.zsh-fzf-tab; 
-          name = "fzf-tab";
-        }
-        { 
-          package = pkgs.zsh-vi-mode;
-          init = lib.optionalString 
-            self.wrappers.zsh.fzf.enable 
-            "zvm_after_init_commands+=('source <(fzf --zsh)')";
-        } 
-        { 
-          package = pkgs.oh-my-zsh;
-          file = "plugins/git/git.plugin.zsh";
-          disable = false;
-        } 
+    zsh = { pkgs, wlib, lib, ... }: 
+    let
+      direnvWrapper = withSystem pkgs.stdenv.hostPlatform.system (
+        { config, ... }: config.packages.direnv
+      );
+      zshWrapperModule = import (self + /wrapper-modules/zsh);
+      zshWrapperConfig = import (self + /config.nix) { direnv = direnvWrapper; };
+    in 
+    {
+      imports = [ 
+        zshWrapperModule
+        zshWrapperConfig
       ];
     };
   };
