ce47143d65cd0afd245af0f2f8bf3d97805ece67
Olivier Bitter <olibitter@gmail.com>
2026-02-26 22:45:56 +0100

    refactor out integrations and plugins

---
+++ wrapper-modules/zsh/integrations.nix
@@ -0,0 +1,32 @@
+{ config, wlib, lib, pkgs, ... }:
+let
+  types = (import ./types) { inherit pkgs lib; };
+  enabledIntegrations = lib.filterAttrs (_: i: i.enable) config.integrations;
+  integrationConfig = lib.concatMapAttrsStringSep 
+    "\n"
+    (name: integration: ''
+      ## ${name} integration
+      ${integration.init}
+    '') enabledIntegrations;
+in
+{
+  options = {
+    integrations = lib.mkOption {
+      default = {};
+      type = lib.types.attrsOf types.integration;
+    };
+    integrationConfig = lib.mkOption {
+      type = lib.types.str;
+      default = integrationConfig;
+      readOnly = true;
+    };
+  };
+  config = {
+    integrations = {
+      fzf.init = lib.mkDefault ''source <(fzf --zsh)'';
+      starship.init = ''eval "$(starship init zsh)"'';
+      direnv.init = ''eval "$(direnv hook zsh)"'';
+    };
+    runtimePackages = lib.mapAttrsToList (_: i : i.package) enabledIntegrations;
+  };
+}
