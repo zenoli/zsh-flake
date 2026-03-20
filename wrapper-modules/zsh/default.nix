df2232d746b3c555e9adc06416b82fc7e03c5999
Olivier Bitter <olibitter@gmail.com>
2026-03-20 16:19:37 +0100

    cleanup

--- wrapper-modules/zsh/default.nix
+++ wrapper-modules/zsh/default.nix
@@ -5,24 +5,6 @@ let
     destination = "/.zshrc";
     text = config.zshrcContent;
   };
-  # zshenv = config.pkgs.writeTextFile {
-  #   name = "zshenv";
-  #   destination = "/.zshenv";
-  #   text = ''
-  #     # Only execute this file once per shell.
-  #     if [ -v "''${__WRAPPED_ZSHENV_SOURCED-}" ]; then return; fi
-  #     __WRAPPED_ZSHENV_SOURCED=1
-  #
-  #     runtime_packages="${pkgs.lib.makeBinPath config.runtimePackages}"
-  #     if [[ $PATH != ''${runtime_packages}:* ]]; then
-  #       export PATH="''${runtime_packages}:$PATH"
-  #     fi
-  #   '';
-  # };
-  # zdotdir = pkgs.symlinkJoin {
-  #   name = "zdotdir";
-  #   paths = [ zshrc zshenv ];
-  # };
   mergedSnippets = lib.concatMapStringsSep "\n\n" 
     (x: lib.trim ''
     # ${x.name}
@@ -38,17 +20,12 @@ in
     ./modules
   ];
   options = {
-    # zdotdir = lib.mkOption {
-    #   type = lib.types.path;
-    #   default = zdotdir;
-    #   readOnly = true;
-    # };
     snippets = lib.mkOption {
       type = wlib.types.dagOf lib.types.str;
     };
-    zshrcContent = lib.mkOption { 
-      type = lib.types.str; 
-      default = ''
+  };
+  config = {
+    zshrc.content = ''
       # Sources a file relative to the src directory
       function load {
         source "${./src}/$1"
@@ -62,30 +39,5 @@ in
 
       ${mergedSnippets}
       '';
-    };
-    # runtimePackages = lib.mkOption {
-    #     type = lib.types.listOf lib.types.package;
-    #     default = [];
-    #     description = ''
-    #       Additional packages added to $PATH in the wrapped .zshenv.
-    #     '';
-    #   };
-    # hmSessionVariables = {
-    #   enable = lib.mkEnableOption "home-manager sessionVariables";
-    #   scriptLocation = lib.mkOption {
-    #     type = lib.types.str;
-    #     description = ''
-    #       Absolute path of the `hm-session-vars.sh` script to be loaded.
-    #     '';
-    #     default = "~/.nix-profile/etc/profile.d/hm-session-vars.sh";
-    #   };
-    # };
-  };
-  config = {
-    zshrc.content = config.zshrcContent;
-    # package = pkgs.zsh;
-    # env = {
-    #   ZDOTDIR = "${zdotdir}";
-    # };
   };
 }
--- wrapper-modules/zsh/types/integration.nix
+++ wrapper-modules/zsh/types/integration.nix
@@ -2,10 +2,6 @@
 lib.types.submodule ({ config, name, ... }: {
   options = {
     enable = lib.mkEnableOption "${name} integration";
-    runtimePackage = lib.mkOption {
-      type = lib.types.package;
-      default = pkgs.${name};
-    };
     init = lib.mkOption {
       type = lib.types.nullOr lib.types.str;
       default = null;
