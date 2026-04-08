18856761d5afd2a10c2db4f4953ff41ef4e2e534
Olivier Bitter <olibitter@gmail.com>
2026-04-08 22:32:47 +0200

    fix edit-command-line mapping in insert mode

--- config/src/init.zsh
+++ config/src/init.zsh
@@ -1,11 +1,10 @@
-KEYTIMEOUT=1
-# VI_MODE_SET_CURSOR=true
-# VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true
-
 autoload -Uz edit-command-line
 zle -N edit-command-line
-bindkey -M vicmd '^f' edit-command-line
-bindkey -M viins '^f' edit-command-line
+
+function zvm_after_init() {
+  bindkey -M vicmd '^f' edit-command-line
+  bindkey -M viins '^f' edit-command-line
+}
 
 load env.zsh
 load options.zsh
