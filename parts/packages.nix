e5df970f51a9dbb7b61f2f2b7ee8fff9be33b563
Olivier Bitter <olibitter@gmail.com>
2026-04-13 23:30:24 +0200

    extract p10k-configure script to flake.packages

--- parts/packages.nix
+++ parts/packages.nix
@@ -1,5 +1,6 @@
 {
   perSystem = { pkgs, self', ... }: {
     packages.default = self'.packages.zsh;
+    packages.p10k-configure = pkgs.callPackage ../scripts/p10k-configure.nix {};
   };
 }
--- shell.nix
+++ shell.nix
@@ -9,6 +9,7 @@ let
   # Define your scripts/aliases
   scripts = [
     (pkgs.callPackage ./scripts/ghd {})
+    (pkgs.callPackage ./scripts/p10k-configure.nix {})
     (pkgs.writeShellApplication {
       name = "debug-zshrc";
       runtimeInputs = with pkgs; [ neovim ];
@@ -23,26 +24,6 @@ let
         nix build && nvim result/zsh-dot-dir/.zshenv
       '';
     })
-    (pkgs.writeScriptBin "p10k-configure" ''
-      #!/usr/bin/env -S ${lib.getExe pkgs.zsh} -i
-      ZDOTDIR=$(${lib.getExe pkgs.mktemp} -d --tmpdir zdotdir-XXXXXXXXXX)
-      export ZDOTDIR
-      source ${pkgs.zsh-powerlevel10k.src}/powerlevel10k.zsh-theme
-      p10k configure
-      read "move?Move $ZDOTDIR/.p10k.zsh? [y/n] "
-      if [[ $move == y ]]; then
-        dest=.
-        vared -p "Destination directory: " dest
-        mv "$ZDOTDIR/.p10k.zsh" "$dest/.p10k.zsh"
-        echo "Moved to $dest/.p10k.zsh"
-        rm -rf "$ZDOTDIR"
-        echo "Removed $ZDOTDIR"
-      else
-        echo ".p10k.zsh was created at:"
-        echo "$ZDOTDIR/.p10k.zsh"
-      fi
-      echo Done!
-    '')
     (pkgs.writeShellApplication {
       name = "dev";
       runtimeInputs = with pkgs; [ watchexec ];
